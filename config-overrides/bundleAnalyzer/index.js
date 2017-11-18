11761dcff6a3869ceabc94d3bee3ce8df43082b4
Aleksei Gurianov <gurianov@gmail.com>
2017-11-18 13:13:07 +0400

    fix: bundle analyzers configs

--- config-overrides/bundleAnalyzer/index.js
+++ config-overrides/bundleAnalyzer/index.js
@@ -5,24 +5,22 @@ module.exports = function rewireBundleAnalyzer(config, env) {
   config.plugins = [
     ...config.plugins,
     // run npx rempl-cli to check report
-    process.env.ANALYZE_RUNTIME === 'true'
-      ? new RuntimeAnalyzerPlugin({
-          mode: 'publisher',
-          watchModeOnly: false
-        })
-      : [],
+    process.env.ANALYZE_RUNTIME === 'true' &&
+      new RuntimeAnalyzerPlugin({
+        mode: 'publisher',
+        watchModeOnly: false
+      }),
     // open bundle/report.html to check report
-    process.env.ANALYZE_BUNDLE === 'true'
-      ? new BundleAnalyzerPlugin({
-          analyzerMode: 'static',
-          reportFilename: 'report.html',
-          defaultSizes: 'parsed',
-          generateStatsFile: false,
-          logLevel: 'info',
-          openAnalyzer: false
-        })
-      : []
-  ]
+    process.env.ANALYZE_BUNDLE === 'true' &&
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: 'report.html',
+        defaultSizes: 'parsed',
+        generateStatsFile: false,
+        logLevel: 'info',
+        openAnalyzer: false
+      })
+  ].filter(Boolean)
 
   return config
 }
