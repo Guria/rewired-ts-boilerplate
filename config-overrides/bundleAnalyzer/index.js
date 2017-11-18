e4c36b6e4fa7546f63dcd5e18c86df0386ac8836
Aleksei Gurianov <gurianov@gmail.com>
2017-11-18 09:13:11 +0400

    feat: separate env vars to enable analyzers

--- config-overrides/bundleAnalyzer/index.js
+++ config-overrides/bundleAnalyzer/index.js
@@ -2,25 +2,27 @@ var RuntimeAnalyzerPlugin = require('webpack-runtime-analyzer')
 var { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = function rewireBundleAnalyzer(config, env) {
-  if (process.env.ANALYZE_BUILD === 'true') {
-    config.plugins = [
-      ...config.plugins,
-      // run npx rempl-cli to check report
-      new RuntimeAnalyzerPlugin({
-        mode: 'publisher',
-        watchModeOnly: true
-      }),
-      // open bundle/report.html to check report
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        reportFilename: 'report.html',
-        defaultSizes: 'parsed',
-        generateStatsFile: false,
-        logLevel: 'info',
-        openAnalyzer: false
-      })
-    ]
-  }
+  config.plugins = [
+    ...config.plugins,
+    // run npx rempl-cli to check report
+    process.env.ANALYZE_RUNTIME === 'true'
+      ? new RuntimeAnalyzerPlugin({
+          mode: 'publisher',
+          watchModeOnly: false
+        })
+      : [],
+    // open bundle/report.html to check report
+    process.env.ANALYZE_BUNDLE === 'true'
+      ? new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: 'report.html',
+          defaultSizes: 'parsed',
+          generateStatsFile: false,
+          logLevel: 'info',
+          openAnalyzer: false
+        })
+      : []
+  ]
 
   return config
 }
--- config-overrides/ts/typescriptTransform.js
+++ config-overrides/ts/typescriptTransform.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const tsJestPreprocessor = require('ts-jest/preprocessor')
 
 module.exports = tsJestPreprocessor
